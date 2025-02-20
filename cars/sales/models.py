abc53ba548e14b6a14df309da24b3652a7071b31
Yabiti <yabetseyohannes@gmail.com>
2025-02-20 03:01:21 -0800

    .

--- cars/sales/models.py
+++ cars/sales/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Cars(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
