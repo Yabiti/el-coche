627a3cdd6fea19d5b4f131cee34aa293d7eeed23
Yabiti <yabetseyohannes@gmail.com>
2025-02-20 03:09:41 -0800

    .

--- cars/sales/models.py
+++ cars/sales/models.py
@@ -8,9 +8,9 @@ class Cars(models.Model):
     year = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     content = models.TextField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='car/')
     published_date = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return self.title
+        return f"{self.make} {self.model} {self.year}"
