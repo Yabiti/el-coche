100348d8f018ace18d93bd0fbc797a30fb5a5123
Yabiti <yabetseyohannes@gmail.com>
2025-02-20 03:17:12 -0800

    .

--- cars/sales/models.py
+++ cars/sales/models.py
@@ -20,3 +20,8 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=50)
     emial = models.EmailField()
     phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
