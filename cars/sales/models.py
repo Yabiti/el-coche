311199e14c3119a665fba3ada8e4fe6c1113aa1b
Yabiti <yabetseyohannes@gmail.com>
2025-02-20 03:23:03 -0800

    .

--- cars/sales/models.py
+++ cars/sales/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Cars(models.Model):
+class Car(models.Model):
     title = models.CharField(max_length=150)
     make = models.CharField(max_length=150)
     model = models.CharField(max_length=150)
@@ -25,3 +25,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class Sale(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    sale_date = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.car} to {self.customer} on {self.sale_date}"
