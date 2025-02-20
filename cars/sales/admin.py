c84c5d92b7405c47229346408bb16858e53dc06a
Yabiti <yabetseyohannes@gmail.com>
2025-02-20 03:42:37 -0800

    .

--- cars/sales/admin.py
+++ cars/sales/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Car, Customer, Sale
 # Register your models here.
+
+admin.site.register(Car)
+admin.site.register(Customer)
+admin.site.register(Sale)
