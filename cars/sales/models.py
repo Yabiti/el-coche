13b188d8f7cba4baa8b10f372bbfe71bd7c1a0c5
Yabiti <yabetseyohannes@gmail.com>
2025-02-20 03:02:27 -0800

    .

--- cars/sales/models.py
+++ cars/sales/models.py
@@ -5,3 +5,4 @@ class Cars(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     image = models.ImageField(null=True, blank=True)
+    published_date = models.DateTimeField(auto_now_add=True)
