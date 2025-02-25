214f877a7e7c7cc2f8aaee219596076dfad3964a
Yabiti <yabetseyohannes@gmail.com>
2025-02-24 22:52:52 -0800

    .

--- cars/sales/views.py
+++ cars/sales/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import Car
 # Create your views here.
+
+class HomePageView(TemplateView):
+    model = Car
+    template_name = 'sales/base.html'
